870ff1fa42a28407dffd4aedc4355280dce69dbb
Juan Adasme <jadasmetoro@gmail.com>
2019-02-07 16:57:26 -0300

    Add aspect

---
+++ src/main/java/io/p23/training/interceptors/ExampleAspect.java
@@ -0,0 +1,20 @@
+package io.p23.training.interceptors;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExampleAspect {
+	
+	@Around("execution(* io.p23.training.controller.*.*(..))")
+	public Object logAroundMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+		Object proceed = joinPoint.proceed();
+		long executionTime = System.currentTimeMillis() - start;
+		System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+		return proceed;
+	}
+}
